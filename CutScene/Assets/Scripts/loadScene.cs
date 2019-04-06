6dcff9a69fd4e82aaaba09cb93b5889ccbe374b5
Nathalia Paiva Lima <npl@cin.ufpe.br>
2019-04-06 14:20:24 -0300

    cutscene

---
+++ CutScene/Assets/Scripts/loadScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class loadScene : MonoBehaviour
+{
+    public Button next;
+
+    // start is called before the first frame update
+    void Start()
+    {
+        next.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //output this to console when button1 or button3 is clicked
+        //Debug.Log("you have clicked the button!");
+        SceneManager.LoadScene("Scene1", LoadSceneMode.Additive);
+        //print("You clicked the button!");
+    }
+    
+    // update is called once per frame
+    //void update()
+    //{
+
+    //}
+}
