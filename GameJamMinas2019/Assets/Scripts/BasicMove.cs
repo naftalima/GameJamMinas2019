0269b2e40b3bad2dcc58864b02b1dc0ba07f9c2f
Nathalia <nathaff@hotmail.com>
2019-04-06 10:07:34 -0300

    project struct
    the grrrl can walk and collide

---
+++ GameJamMinas2019/Assets/Scripts/BasicMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMove : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        //transform.position = transform.position + movement * Time.deltaTime;
+        rb.velocity = new Vector2(movement.x, movement.y);
+
+    }
+
+    /*
+    public void Move()
+    {
+        rb.velocity = new Vector2(movement.x, movement.y);
+    }
+    */
+}
